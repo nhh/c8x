647edfce9577b70aad837d6bd74b386625802165
Niklas Hanft <niklas.hanft@nitrado.net>
2025-02-13 21:34:49 +0100

    Add cross platform c8x binary installation

---
+++ npm/install.cjs
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const os = require('os');
+
+const c8x = JSON.parse(fs.readFileSync("package.json").toString())
+
+const version = c8x.version
+const platform = os.platform()
+const binaryName = platform === "win32" ? `c8x.exe` : `c8x`
+const downloadUrl = `https://github.com/nhh/c8x/releases/download/v${version}-alpha/c8x-${platform}-x86_64${platform === "win32" ? ".exe" : ""}`
+
+fetch(downloadUrl)
+    .then(res => res.status === 200 ? res : Promise.reject(`Download failed: ${res.status}/ ${res.statusText}`))
+    .then(res => res.arrayBuffer())
+    .then(bytes => fs.writeFileSync(binaryName, new Uint8Array(bytes)))
+    .catch(e => console.error(e));
+
+fs.chmodSync(binaryName, fs.constants.S_IRWXU)
